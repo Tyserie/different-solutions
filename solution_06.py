c19ebb90fec460ff2143478d4b3acf7a75192bc7
god <tomas.tyser@seznam.cz>
2020-05-11 05:22:07 -0700

    adding new solution

--- solution_06.py
+++ solution_06.py
@@ -4,7 +4,6 @@ For example:
 
 unsorted_list = [('first_element', 4), ('second_element', 2), ('third_element', 6)]
 sorted_list = [('second_element', 2), ('first_element', 4), ('third_element', 6)]
-
 '''
 
 unsorted_list = [('first_element', 4), ('second_element', 2), ('third_element', 6)]
